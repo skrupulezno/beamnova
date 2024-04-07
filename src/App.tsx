b67ffbd29aa3027b90e5190304ad23ede113c97b
maxim <skrupulezno@gmail.com>
2024-04-07 23:08:46 +0900

    prefinal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React, { Children } from 'react';
 import './App.css';
 import Planet from './Planet';
 import Button from './Button';
@@ -11,7 +10,14 @@ function App() {
       </div>
       <div className="main">
         <header className='header'>
-          <Button>{"hello"}</Button>
+          <div className="header-container">
+            <Button title='О нас' style='about'/>
+            <Button title='Наши проекты' style='project'/>
+          </div>
+          <div className="header-container">
+            <Button title='Следите за нами' style='blog'/>
+            <Button title='Вход' style='auth'/>
+          </div>
         </header>
         <div className="main-content">
           <div className="card">
@@ -22,28 +28,36 @@ function App() {
               Veniam ad ea aliquam eius magni soluta dolor atque quaerat.
             </p>
           </div>
-          <div className="card">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Quia rem nobis voluptas nostrum, autem minus beatae ratione labore qui praesentium, dicta id sunt voluptatem quo!
-              Quaerat quas deleniti ab doloribus quae sunt consequuntur eos eaque iste modi minima, qui voluptates?
-              Veniam ad ea aliquam eius magni soluta dolor atque quaerat.
-            </p>
-          </div>
-          <div className="card">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Quia rem nobis voluptas nostrum, autem minus beatae ratione labore qui praesentium, dicta id sunt voluptatem quo!
-              Quaerat quas deleniti ab doloribus quae sunt consequuntur eos eaque iste modi minima, qui voluptates?
-              Veniam ad ea aliquam eius magni soluta dolor atque quaerat.
-            </p>
-          </div>
 
+          <div className="card">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Quia rem nobis voluptas nostrum, autem minus beatae ratione labore qui praesentium, dicta id sunt voluptatem quo!
+              Quaerat quas deleniti ab doloribus quae sunt consequuntur eos eaque iste modi minima, qui voluptates?
+              Veniam ad ea aliquam eius magni soluta dolor atque quaerat.
+            </p>
+          </div>
+          
+          <div className="card">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Quia rem nobis voluptas nostrum, autem minus beatae ratione labore qui praesentium, dicta id sunt voluptatem quo!
+              Quaerat quas deleniti ab doloribus quae sunt consequuntur eos eaque iste modi minima, qui voluptates?
+              Veniam ad ea aliquam eius magni soluta dolor atque quaerat.
+            </p>
+            <div className="card">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Quia rem nobis voluptas nostrum, autem minus beatae ratione labore qui praesentium, dicta id sunt voluptatem quo!
+              Quaerat quas deleniti ab doloribus quae sunt consequuntur eos eaque iste modi minima, qui voluptates?
+              Veniam ad ea aliquam eius magni soluta dolor atque quaerat.
+            </p>
+          </div>
+          </div>
         </div>
         <footer className='footer'>
-          <div className="container">Copyright &copy; 2024 beamnova</div>        
+          <div className="container"><p>Copyright &copy; 2024 beamnova</p></div>        
         </footer>
-        
       </div>
     </div>
   );
--- src/Button.tsx
+++ src/Button.tsx
@@ -1,12 +1,50 @@
 import './Button.css';
-import React, { ReactNode } from "react";
+import { HiOutlineInformationCircle } from "react-icons/hi2";
+import { AiOutlineQuestionCircle } from "react-icons/ai";
+import { PiSignInBold } from "react-icons/pi";
+import { CgProfile } from "react-icons/cg";
+import { BsBookmarkStar } from "react-icons/bs";
+import { BiNews } from "react-icons/bi";
+import { RiGitRepositoryLine } from "react-icons/ri";
 
-interface Props {
-    children?: ReactNode
-    // any props that come into the component
+const Button = ({ title, style }: { title: string, style : string }) => {
+    if (style === 'about' ) return (
+        <button className='About us'><AiOutlineQuestionCircle style={{
+            color: "rgb(249, 115, 22)",
+            height: "1.3em",
+            width: "1.3em"
+          }}/>{title}</button>
+    );
+    else if (style === 'project' ) return (
+        <button className='Project'><RiGitRepositoryLine style={{
+            color: "rgb(244, 63, 94)",
+            height: "1.3em",
+            width: "1.3em"
+          }}/>{title}</button>
+    );
+    else if (style === 'blog' ) return (
+        <button className='Blog'><BiNews style={{
+            color: "rgb(16, 185, 129)",
+            height: "1.4em",
+            width: "1.4em"
+          }}/>{title}</button>
+    );
+    else if (style === 'auth' ) return (
+        <button className='Auth'><PiSignInBold style={{
+            color: "rgb(219, 165, 9)",
+            height: "1.3em",
+            width: "1.3em"
+          }}/> {title}</button>
+    );
+    else if (style === 'profile' ) return (
+        <button className='Profile'><PiSignInBold />{title}</button>
+    );
+    return (
+        <button className='Standart'>{title}</button>
+    );
 }
 
-const Button = ({ children, ...props }: Props) => (
-    <button {...props}>{children}</button>
-);
+
+
+
 export default Button;
